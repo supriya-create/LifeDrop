22fc8007c8ee5b2f236da47c2b3e06508ef79636
supriya_sinha <supriyasinha.dar@gmail.com>
2026-06-13 18:09:48 +0530

    Add root health route confirmation

--- server.js
+++ server.js
@@ -79,3 +79,7 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("LifeDrop Backend is Running!");
+});
